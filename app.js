ca1cea54a3ab5637fb307a20e2eea17c9e6be7de
shaharyaarv2 <shaharyaarzayer@gmail.com>
2024-11-09 13:52:48 +0000

    Added Update Feature in and also  made the document update in database

--- app.js
+++ app.js
@@ -4,6 +4,9 @@ const ejs = require('ejs');
 const fs = require('fs');
 const path = require('path');
 const Notes = require('./models/notes');
+const mongoose = require('mongoose')
+mongoose.connect('mongodb://127.0.0.1:27017/notes');
+
 
 app.set("view engine" , "ejs");
 app.use(express.json());
@@ -17,28 +20,41 @@ app.get('/' , (req , res) =>{
     })
 });
 
+//Create Routes
+
 app.get('/create' , (req , res) =>{
     res.render('create');
 })
 
-app.get('/edit/:filename' , (req , res)=>{
-    // console.log(req.params)
-    const {filename} = req.params;
-    res.render('edit', {filename})
+app.post('/create' , async(req , res) => {
+    console.log(req.body);
+    const {filename , content } = req.body;
+        const newNote =  new Notes({
+            filename : filename,
+            content : content
+        })
+
+       await newNote.save();
+    res.redirect('/')
 })
-app.post('/edit/:filename' , (req , res) =>{
+
+app.get('/edit/:filename' , async (req , res)=>{
+    const { filename } = req.params;
+    // Find the document using the filename
+    const note = await Notes.findOne({ filename : filename });
+    if (!note) {
+        return res.status(404).send('Note not found');
+    }
+
+    res.render('edit', {filename , content : note.content})
+})
+app.post('/edit/:filename' , async (req , res) =>{
     const {filename} = req.params
-    console.log(filename)
-    const {newName} = (req.body);
-    fs.rename(`./files/${filename}` , `./files/${newName}.txt` , (err)=>{
-      if(err){
-        console.log(err);
-        res.redirect('/');
-      }else{
-        console.log('name changed');
-        res.redirect('/');
-      } 
-    })
+    console.log("request to change the notes of ",filename , "things to change" , req.body);
+    const {newName , newContent} = req.body;
+    const newNote = await Notes.findOneAndUpdate({filename : filename} , {filename : newName , content : newContent});
+    res.redirect('/');
+    
 })
 
 app.get('/edit/:filename' , (req , res) =>{
@@ -52,17 +68,7 @@ app.get('/edit/:filename' , (req , res) =>{
     })
 })
 
-app.post('/create' , async(req , res) => {
-    console.log(req.body);
-    const {filename , content } = req.body;
-        const newNote =  new Notes({
-            fileName : filename,
-            content : content
-        })
 
-       await newNote.save();
-    res.redirect('/')
-})
 
 
 
--- models/notes.js
+++ models/notes.js
@@ -2,9 +2,11 @@ const mongoose = require('mongoose');
 
 mongoose.connect('mongodb://127.0.0.1:27017/notes');
 
-const noteSchema = mongoose.Schema( { fileName: String , content : String  });
+const noteSchema = mongoose.Schema( { filename: String , content : String  });
+
 
 const Note = mongoose.model('Note' , noteSchema);
+
 
 module.exports = Note;
 
